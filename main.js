ea38ee60afe79818dd4be52e3fa7be553d6eea18
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-07 00:31:18 +0500

    masaal-1

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // 1-masala
+
+// let arr = [4, 2, 9, 7, 5];
+// let result = arr.sort(length);
+
+// console.log(result);
+
+
+
+///5-masala
+function arrnum(array) {
+    let result = [];
+
+    for (let value of array) {
+        if (!result.includes(value)) {
+            result.push(value); 
+        }
+    }
+
+    return result.length === 2;
+}
+let array1 = [1, 2, 1, 2, 1];
+console.log(arrnum(array1));
