9def2e12a9f66751345f483d01101f1143b2e102
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-07 12:05:27 +0500

    bdsk

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-// 1-masala
+///1-masala
+// function arrnum(array) {
+//     let result = [];
+
+//     for (let value of array) {
+//         if (!result.includes(value)) {
+//             result.push(value);
+//         }
+//     }
+
+//     return result.length === 2;
+// }
+// let array1 = [1, 2, 1, 2, 1];
+// console.log(arrnum(array1));
+
+// 2-masala
 
 // let arr = [4, 2, 9, 7, 5];
-// let result = arr.sort(length);
+// let result = arr.sort((a, b) =>b-a);
+// n=2
+// console.log(result[result.length - n]);
 
-// console.log(result);
 
 
-
-///5-masala
-function arrnum(array) {
-    let result = [];
-
-    for (let value of array) {
-        if (!result.includes(value)) {
-            result.push(value); 
-        }
-    }
-
-    return result.length === 2;
-}
-let array1 = [1, 2, 1, 2, 1];
-console.log(arrnum(array1));
+//5-masala
+// function num(array) {
+//   const result = [];
+//   for (let key of array) {
+//     if (!result.includes(key)) {
+//       result.push(key);
+//     }
+//     if (result.length > 2) {
+//       return false;
+//     }
+//   }
+//   return result.length === 2;
+// }
+// console.log(num([1, 1, 2, 2, 2]));
